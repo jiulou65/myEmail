2165bf5e6e8db870ffad3a81d5d4835eb82e9524
qian | money <58126801+jiulou65@users.noreply.github.com>
2021-05-09 22:32:01 +0800

    优化定时发送

--- src/main/java/com/email/demo/config/EmailQuartz.java
+++ src/main/java/com/email/demo/config/EmailQuartz.java
@@ -5,13 +5,18 @@ import com.email.demo.service.EmailService;
 import com.email.demo.utils.MessageUtil;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
 public class EmailQuartz extends QuartzJobBean {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private EmailService emailService;
@@ -22,9 +27,14 @@ public class EmailQuartz extends QuartzJobBean {
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         List<MailMessage> mailMessages = emailService.queryEmail();
-        Iterator<MailMessage> iterator = mailMessages.iterator();
-        while (iterator.hasNext()){
-            messageUtil.sendMyEmail(iterator.next());
+        if (mailMessages != null && mailMessages.size() > 0){
+            Iterator<MailMessage> iterator = mailMessages.iterator();
+            while (iterator.hasNext()){
+                messageUtil.sendMyEmail(iterator.next());
+            }
+        } else {
+            logger.info("数据库暂无数据,"+new Date());
         }
+
     }
 }
