5129b925a2bb15fcf5da3f5632140cc0bef5cd31
jiulou65 <1665222493@qq.com>
2021-07-17 17:44:43 +0800

    初始化

--- src/main/java/com/email/demo/DemoApplication.java
+++ src/main/java/com/email/demo/DemoApplication.java
@@ -12,6 +12,9 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("gogogo");
+        System.out.println("gogogo");
+        System.out.println("gogogo");
+        System.out.println("gogogo");
     }
 
 }
